fe852a9e6e3852955a48d3c4147f0deeda47b573
arshitapatel077@gmail.com <arshitapatel077@gmail.com>
2025-05-02 17:04:02 +0530

    added file

---
+++ FigletDir/index.js
@@ -0,0 +1,11 @@
+const figlet = require("figlet");
+
+
+figlet("Jay & Arshita", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
